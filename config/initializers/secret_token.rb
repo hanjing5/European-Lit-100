86fa6af91ddd7766d74312ce3bf82c5d663e451e
Han Jing <hanqijing@gmail.com>
2012-05-01 17:30:27 -0700

    first commit for flash sale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EuropeanLit100::Application.config.secret_token = '152ff0a415206513b7773ab14884cf8590b6b5e02595886abaf2c8004a62a3c4d3c3396c0bcc47b9b65e0012d19f12fbd90b1842eae8b8a2292f5c95db2f15ae'
